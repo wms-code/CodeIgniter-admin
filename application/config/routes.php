781b865049d34a9385cecfb78af463ccb1ef4857
satheeshdomainindia <satheesh@dreamstudio.in>
2015-12-20 15:01:06 +0530

    Added  Admin theme
    Added  Admin theme

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = "home";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$controller_exceptions = array('admin','user','login');
+$route["^((?!\b".implode('\b|\b', $controller_exceptions)."\b).*)$"] = $route['default_controller'].'/$1';
+$route['admin'] = 'admin/home';
+
